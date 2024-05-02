6ede2271e27987212a40a5ecd60612e821aef87a
salem83 <toaste@gmx.net>
2024-05-02 17:52:14 +0200

    Fixed icons and cleanup
    BBV is currently not possible https://github.com/betaflight/blackbox-log-viewer/issues/737

--- create_installer.sh
+++ create_installer.sh
@@ -30,10 +30,15 @@ BBL_URL="https://github.com/betaflight/blackbox-log-viewer.git"
 function _L() {
   # before nl
   if [ -n "$2" ] && [ "$2" == "1" ]; then MSG="\n"; fi;
-  MSG="${MSG}${1}"
+  MSG="${MSG}\n${1}\n"
   # after nl  
   if [ -n "$3" ] && [ "$3" == "1" ]; then MSG="${MSG}\n"; fi;
   echo -e $MSG
+}
+
+
+function _P() {
+  read -p "Press ENTER key ..."
 }
 
 
@@ -63,6 +68,7 @@ if [ -d "$BFC_DIR/.git" ]; then
 else
   git clone $BFC_URL
 fi;
+
 yarn --cwd $BFC_DIR install
 
 _L " - blackbox-log-viewer" 1
@@ -81,7 +87,7 @@ _L " - betaflight-configurator" 1
 yarn --cwd betaflight-configurator gulp dist
 
 _L " - blackbox-log-viewer" 1
-yarn --cwd blackbox-log-viewer gulp dist
+yarn --cwd blackbox-log-viewer build
 
 
 # get tools / resources -----------------------------------------
@@ -91,33 +97,36 @@ mkdir -p $WINE_DIR
 ls $WINE_DIR
 
 _L " - resource hacker" 1
-rm -rf resource_hacker.zip*
-wget $RH_URL
+# rm -rf resource_hacker.zip*
+if [ ! -f "resource_hacker.zip"]; then
+  wget $RH_URL
+fi
 unzip -o -d $WINE_DIR ./resource_hacker.zip "*.exe"
 
 _L " - nw.js" 1
-rm -rf nwjs-*
-wget "${NW_URL}${NW_ZIP}"
-unzip -o -d $WINE_DIR $NW_ZIP
+# rm -rf nwjs-*
 
+if [ ! -f "${NW_ZIP}"]; then
+  wget "${NW_URL}${NW_ZIP}"
+fi
+unzip -o -d $WINE_DIR $NW_ZIP
 
 
 # pack it -------------------------------------------------------
 _L " --- PACK IT ---" 1
 
-pushd .
-
 # bf
-_L $BFC_DIR 1
+pushd .
+_L " - betaflight configurator" 1
 cd betaflight-configurator/dist
 zip -r "${WINE_DIR}/${NW_REL}/bfc.nw" *
 popd
 
 # black box
-_L " - blackbox log viewer" 1
 pushd .
+_L " - blackbox log viewer" 1
 cd blackbox-log-viewer/dist
-zip -r "${WINE_DIR}/${NW_REL}/bfbbv.nw" *
+zip -r "${WINE_DIR}/${NW_REL}/bbv.nw" *
 popd
 
 
@@ -128,13 +137,24 @@ _L " - betaflight-configurator" 1
 wine64 cmd "/c copy /b ${WINE_NW}nw.exe+${WINE_NW}bfc.nw ${WINE_NW}bfc.exe"
 
 _L " - blackbox-log-viewer" 1
-wine64 cmd "/c copy /b ${WINE_NW}nw.exe+${WINE_NW}bfbbv.nw ${WINE_NW}bfbbv.exe"
+wine64 cmd "/c copy /b ${WINE_NW}nw.exe+${WINE_NW}bbv.nw ${WINE_NW}bbv.exe"
 
 
 ## change icons --------------------------------------------------
 echo " - change icons"
-cp ./bf.ico $WINE_DIR$NW_REL/bf.ico
-cp ./bb.ico $WINE_DIR$NW_REL/bb.ico
+
+rm -rf *.bmp
+rm -rf *.ico
+
+wget https://raw.githubusercontent.com/betaflight/betaflight-configurator/master/assets/windows/bf_installer_small.bmp -O bfc.bmp
+convert -background transparent "bfc.bmp" -define icon:auto-resize=16,24,32,48,64,72,96,128 "bfc.ico"
+
+# wget https://raw.githubusercontent.com/betaflight/blackbox-log-viewer/master/assets/windows/bf_installer_small.bmp -o bbl.bmp
+wget https://raw.githubusercontent.com/betaflight/blackbox-log-viewer/3.7.0_NWjs_maintenance/assets/windows/bf_installer_small.bmp -O bbv.bmp
+convert -background transparent "bbv.bmp" -define icon:auto-resize=16,24,32,48,64,72,96,128 "bbv.ico"
+
+cp ./bfc.ico $WINE_DIR$NW_REL/bfc.ico
+cp ./bbv.ico $WINE_DIR$NW_REL/bbv.ico
 
 
 # merge icons
@@ -142,8 +162,8 @@ _L " --- ICONS ---"
 pushd .
 
 cd $WINE_DIR
-wine64 ResourceHacker.exe -open "${WINE_NW}bfc.exe" -save "${WINE_NW}bfc.exe" -action addoverwrite -resource "${WINE_NW}bf.ico" -mask ICONGROUP,IDR_MAINFRAME
-wine64 ResourceHacker.exe -open "${WINE_NW}bfbbv.exe" -save "${WINE_NW}bfbbv.exe" -action addoverwrite -resource "${WINE_NW}bb.ico" -mask ICONGROUP,IDR_MAINFRAME
+wine64 ResourceHacker.exe -open "${WINE_NW}bfc.exe" -save "${WINE_NW}bfc.exe" -action addoverwrite -resource "${WINE_NW}bfc.ico" -mask ICONGROUP,IDR_MAINFRAME
+wine64 ResourceHacker.exe -open "${WINE_NW}bbv.exe" -save "${WINE_NW}bbv.exe" -action addoverwrite -resource "${WINE_NW}bbv.ico" -mask ICONGROUP,IDR_MAINFRAME
 
 popd
 
@@ -158,17 +178,17 @@ rm -rf *.nw
 rm -rf nw.exe
 rm -rf *.ico
 
-zip -r $CDIR/release.zip * 
+zip -r $CDIR/release.zip *
 
 popd
 
 
 ## clean up
 _L "--- clean up ---"
-pwd
 rm -rf $WINE_DIR
-rm -rf $NW_ZIP
-rm -rf resource_hacker.zip
-
+# rm -rf $NW_ZIP
+# rm -rf resource_hacker.zip
+rm *.ico
+rm *.bmp
 
 # --- EOF ---
